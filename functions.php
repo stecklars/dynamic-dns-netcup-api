971018c6b0726b75af7e214ccdc0b59eb11811bb
Timo Förster <tfoerster@webfoersterei.de>
2018-05-30 22:45:41 +0200

    Improve codestyle: PSR-2-like (see https://www.php-fig.org/psr/psr-2/)

--- functions.php
+++ functions.php
@@ -1,153 +1,164 @@
 <?php
 
-  require_once("config.php");
+require_once 'config.php';
 
-  // Sends $request to netcup Domain API and returns the result
-  function sendRequest($request) {
+const SUCCESS = 'success';
+
+// Sends $request to netcup Domain API and returns the result
+function sendRequest($request)
+{
     $ch = curl_init(APIURL);
     $curlOptions = array(
-      CURLOPT_POST => 1,
-      CURLOPT_RETURNTRANSFER => 1,
-      CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
-      CURLOPT_POSTFIELDS => $request
+        CURLOPT_POST => 1,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_HTTPHEADER => array('Content-Type: application/json'),
+        CURLOPT_POSTFIELDS => $request,
     );
     curl_setopt_array($ch, $curlOptions);
 
     $result = curl_exec($ch);
     curl_close($ch);
 
-    $result = json_decode($result, True);
-    return $result;
-  }
+    $result = json_decode($result, true);
 
-  //Returns current public IP.
-  function getCurrentPublicIP() {
+    return $result;
+}
+
+//Returns current public IP.
+function getCurrentPublicIP()
+{
     $publicIP = file_get_contents('https://api.ipify.org');
 
     //Let's check that this is really a IPv4 address, just in case...
     if (filter_var($publicIP, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4)) {
-      return($publicIP);
+        return $publicIP;
     }
-    else {
-      echo("https://api.ipify.org didn't return a valid IPv4 address.\n\n");
-      return(False);
-    }
-  }
 
-  //Login into netcup domain API and returns Apisessionid
-  function login($customernr, $apikey, $apipassword) {
+    echo("https://api.ipify.org didn't return a valid IPv4 address.\n\n");
 
+    return false;
+}
+
+//Login into netcup domain API and returns Apisessionid
+function login($customernr, $apikey, $apipassword)
+{
     $logindata = array(
-      'action' => 'login',
-      'param' =>
-        array(
-        'customernumber' => CUSTOMERNR,
-        'apikey' => APIKEY,
-        'apipassword' => APIPASSWORD
-        )
+        'action' => 'login',
+        'param' =>
+            array(
+                'customernumber' => CUSTOMERNR,
+                'apikey' => APIKEY,
+                'apipassword' => APIPASSWORD,
+            ),
     );
 
     $request = json_encode($logindata);
 
     $result = sendRequest($request);
 
-    if ($result['status'] == "success") {
-      return($result['responsedata']['apisessionid']);
+    if ($result['status'] === SUCCESS) {
+        return $result['responsedata']['apisessionid'];
     }
-    else {
-      echo("ERROR: Error while logging in: " . $result['longmessage'] . "\n\n");
-      return(False);
-    }
-  }
-  //Logout of netcup domain API, returns boolean
-  function logout($customernr, $apikey, $apisessionid) {
+
+    printf("ERROR: Error while logging in: %s \n\n", $result['longmessage']);
+
+    return false;
+}
+
+//Logout of netcup domain API, returns boolean
+function logout($customernr, $apikey, $apisessionid)
+{
 
     $logoutdata = array(
-      'action' => 'logout',
-      'param' =>
-        array(
-          'customernumber' => CUSTOMERNR,
-          'apikey' => APIKEY,
-          'apisessionid' => $apisessionid
-        )
+        'action' => 'logout',
+        'param' =>
+            array(
+                'customernumber' => CUSTOMERNR,
+                'apikey' => APIKEY,
+                'apisessionid' => $apisessionid,
+            ),
     );
 
     $request = json_encode($logoutdata);
 
     $result = sendRequest($request);
 
-    if ($result['status'] == "success") {
-      return(True);
+    if ($result['status'] === 'success') {
+        return (true);
     }
-    else {
-      echo("ERROR: Error while logging out: " . $result['longmessage'] . "\n\n");
-      return(False);
-    }
-  }
-  //Get info about dns zone from netcup domain API, returns result
-  function infoDnsZone($domainname, $customernr, $apikey, $apisessionid) {
+
+    printf("ERROR: Error while logging out: %s\n\n", $result['longmessage']);
+
+    return false;
+}
+
+//Get info about dns zone from netcup domain API, returns result
+function infoDnsZone($domainname, $customernr, $apikey, $apisessionid)
+{
     $infoDnsZoneData = array(
-      'action' => 'infoDnsZone',
-      'param' =>
-        array(
-          'domainname' => $domainname,
-          'customernumber' => $customernr,
-          'apikey' => $apikey,
-          'apisessionid' => $apisessionid
-        )
+        'action' => 'infoDnsZone',
+        'param' =>
+            array(
+                'domainname' => $domainname,
+                'customernumber' => $customernr,
+                'apikey' => $apikey,
+                'apisessionid' => $apisessionid,
+            ),
     );
 
     $request = json_encode($infoDnsZoneData);
 
     $result = sendRequest($request);
 
-    if ($result['status'] == "success") {
-      return($result);
+    if ($result['status'] === SUCCESS) {
+        return $result;
     }
-    else {
-      echo("Error while getting DNS Zone info: " . $result['longmessage'] . "\n\n");
-      return(False);
-    }
-  }
 
-  //Get info about dns records from netcup domain API, returns result
-  function infoDnsRecords($domainname, $customernr, $apikey, $apisessionid) {
+    printf("Error while getting DNS Zone info: %s\n\n", $result['longmessage']);
+
+    return false;
+}
+
+//Get info about dns records from netcup domain API, returns result
+function infoDnsRecords($domainname, $customernr, $apikey, $apisessionid)
+{
     $infoDnsRecordsData = array(
-      'action' => 'infoDnsRecords',
-      'param' =>
-        array(
-          'domainname' => $domainname,
-          'customernumber' => $customernr,
-          'apikey' => $apikey,
-          'apisessionid' => $apisessionid
-        )
+        'action' => 'infoDnsRecords',
+        'param' =>
+            array(
+                'domainname' => $domainname,
+                'customernumber' => $customernr,
+                'apikey' => $apikey,
+                'apisessionid' => $apisessionid,
+            ),
     );
 
     $request = json_encode($infoDnsRecordsData);
 
     $result = sendRequest($request);
 
-    if ($result['status'] == "success") {
-      return($result);
+    if ($result['status'] === SUCCESS) {
+        return $result;
     }
-    else {
-      echo("Error while getting DNS Record info: " . $result['longmessage'] . "\n\n");
-      return(False);
-    }
-  }
 
-  //Updates DNS Zone using the netcup domain API and returns boolean
-  function updateDnsZone($domainname, $customernr, $apikey, $apisessionid, $dnszone) {
+    printf("Error while getting DNS Record info: %s\n\n", $result['longmessage']);
+
+    return false;
+}
+
+//Updates DNS Zone using the netcup domain API and returns boolean
+function updateDnsZone($domainname, $customernr, $apikey, $apisessionid, $dnszone)
+{
     $updateDnsZoneData = array(
-      'action' => 'updateDnsZone',
-      'param' =>
-        array(
-          'domainname' => $domainname,
-          'customernumber' => $customernr,
-          'apikey' => $apikey,
-          'apisessionid' => $apisessionid,
-          'dnszone' => $dnszone
-        )
+        'action' => 'updateDnsZone',
+        'param' =>
+            array(
+                'domainname' => $domainname,
+                'customernumber' => $customernr,
+                'apikey' => $apikey,
+                'apisessionid' => $apisessionid,
+                'dnszone' => $dnszone,
+            ),
     );
 
     $request = json_encode($updateDnsZoneData);
@@ -155,39 +166,40 @@
     $result = sendRequest($request);
 
     if ($result['status'] == "success") {
-      return(True);
+        return true;
     }
-    else {
-      echo("Error while updating DNS Zone: " . $result['longmessage'] . "\n\n");
-      return(False);
-    }
-  }
 
-  //Updates DNS records using the netcup domain API and returns boolean
-  function updateDnsRecords($domainname, $customernr, $apikey, $apisessionid, $dnsrecords) {
+    printf("Error while updating DNS Zone: %s\n\n", $result['longmessage']);
+
+    return false;
+}
+
+//Updates DNS records using the netcup domain API and returns boolean
+function updateDnsRecords($domainname, $customernr, $apikey, $apisessionid, $dnsrecords)
+{
     $updateDnsZoneData = array(
-      'action' => 'updateDnsRecords',
-      'param' =>
-        array(
-          'domainname' => $domainname,
-          'customernumber' => $customernr,
-          'apikey' => $apikey,
-          'apisessionid' => $apisessionid,
-          'dnsrecordset' => array(
-            'dnsrecords' => $dnsrecords
-          )
-        )
+        'action' => 'updateDnsRecords',
+        'param' =>
+            array(
+                'domainname' => $domainname,
+                'customernumber' => $customernr,
+                'apikey' => $apikey,
+                'apisessionid' => $apisessionid,
+                'dnsrecordset' => array(
+                    'dnsrecords' => $dnsrecords,
+                ),
+            ),
     );
 
     $request = json_encode($updateDnsZoneData);
 
     $result = sendRequest($request);
 
-    if ($result['status'] == "success") {
-      return(True);
+    if ($result['status'] === SUCCESS) {
+        return true;
     }
-    else {
-      echo("Error while updating DNS Records: " . $result['longmessage'] . "\n\n");
-      return(False);
-    }
-  }
+
+    printf("Error while updating DNS Records: %s\n\n", $result['longmessage']);
+
+    return false;
+}
--- update.php
+++ update.php
@@ -1,121 +1,110 @@
 <?php
 
-  require_once("functions.php");
+require_once 'functions.php';
 
-  echo("===============================================================\n");
-  echo("Running dynamic DNS client for netcup 1.0 at " . date("Y/m/d H:i:s") . "\n");
-  echo("This script is not affiliated with netcup.\n");
-  echo("===============================================================\n\n");
+echo "===============================================================\n";
+echo "Running dynamic DNS client for netcup 1.0 at ".date("Y/m/d H:i:s")."\n";
+echo "This script is not affiliated with netcup.\n";
+echo "===============================================================\n\n";
 
-  // Login
-  if ($apisessionid = login(CUSTOMERNR, APIKEY, APIPASSWORD)) {
+// Login
+if ($apisessionid = login(CUSTOMERNR, APIKEY, APIPASSWORD)) {
     echo "Logged in successfully!\n\n";
-  }
-  else {
+} else {
     die("Exiting due to fatal error...\n\n");
-  }
+}
 
-  // Let's get infos about the DNS zone
-  if ($infoDnsZone = infoDnsZone(DOMAIN, CUSTOMERNR, APIKEY, $apisessionid)) {
+// Let's get infos about the DNS zone
+if ($infoDnsZone = infoDnsZone(DOMAIN, CUSTOMERNR, APIKEY, $apisessionid)) {
     echo "Successfully received Domain info.\n\n";
-  }
-  else {
+} else {
     die("Exiting due to fatal error...\n\n");
-  }
-  //TTL Warning
-  if ($infoDnsZone['responsedata']['ttl'] > 300 && CHANGE_TTL != True) {
-    echo("TTL is higher than 300 seconds - this is not optimal for dynamic DNS, since DNS updates will take a long time. Ideally, change TTL to lower value. You may set CHANGE_TTL to True in config.php,
-in which case TTL will be set to 300 seconds automatically.\n\n");
-  }
+}
+//TTL Warning
+if (CHANGE_TTL !== true && $infoDnsZone['responsedata']['ttl'] > 300) {
+    echo "TTL is higher than 300 seconds - this is not optimal for dynamic DNS, since DNS updates will take a long time. Ideally, change TTL to lower value. You may set CHANGE_TTL to True in config.php,\nin which case TTL will be set to 300 seconds automatically.\n\n";
+}
 
-  //If user wants it, then we lower TTL, in case it doesn't have correct value
-  if (CHANGE_TTL == True && $infoDnsZone['responsedata']['ttl'] != 300) {
-
+//If user wants it, then we lower TTL, in case it doesn't have correct value
+if (CHANGE_TTL === true && $infoDnsZone['responsedata']['ttl'] !== 300) {
     $infoDnsZone['responsedata']['ttl'] = 300;
 
     if (updateDnsZone(DOMAIN, CUSTOMERNR, APIKEY, $apisessionid, $infoDnsZone['responsedata'])) {
-      echo("Lowered TTL to 300 seconds successfully.\n\n");
+        echo "Lowered TTL to 300 seconds successfully.\n\n";
+    } else {
+        echo "Failed to set TTL...\n\n";
     }
-    else {
-      echo("Failed to set TTL...\n\n");
-    }
-  }
+}
 
-  //Let's get the DNS record data.
-  if ($infoDnsRecords = infoDnsRecords(DOMAIN, CUSTOMERNR, APIKEY, $apisessionid)) {
-    echo ("Successfully received DNS record data.\n\n");
-  }
-  else {
+//Let's get the DNS record data.
+if ($infoDnsRecords = infoDnsRecords(DOMAIN, CUSTOMERNR, APIKEY, $apisessionid)) {
+    echo "Successfully received DNS record data.\n\n";
+} else {
     die("Exiting due to fatal error...\n\n");
-  }
+}
 
-  //Find the ID of the Host(s)
-  $hostIDs = array();
+//Find the ID of the Host(s)
+$hostIDs = array();
 
-  foreach ($infoDnsRecords['responsedata']['dnsrecords'] as $record) {
+foreach ($infoDnsRecords['responsedata']['dnsrecords'] as $record) {
     if ($record['hostname'] == HOST && $record['type'] == "A") {
-      array_push($hostIDs, array(
-        'id' => $record['id'],
-        'hostname' => $record['hostname'],
-        'type' => $record['type'],
-        'priority' => $record['priority'],
-        'destination' => $record['destination'],
-        'deleterecord' => $record['deleterecord'],
-        'state' => $record['state']
-      ));
+        $hostIDs[] = array(
+            'id' => $record['id'],
+            'hostname' => $record['hostname'],
+            'type' => $record['type'],
+            'priority' => $record['priority'],
+            'destination' => $record['destination'],
+            'deleterecord' => $record['deleterecord'],
+            'state' => $record['state'],
+        );
     }
-  }
+}
 
-  //If we can't find the zone, exit due to error.
-  if (sizeof($hostIDs) == 0) {
+//If we can't find the zone, exit due to error.
+if (count($hostIDs) === 0) {
     // TODO: Add Host
-    die("Error: Host ". HOST . " with an A-Record doesn't exist! Exiting...\n\n");
-  }
+    die("Error: Host ".HOST." with an A-Record doesn't exist! Exiting...\n\n");
+}
 
-  //If the host with A record exists more than one time...
-  if (sizeof($hostIDs) > 1) {
-    echo ("Found multiple A-Records for the Host " . HOST . ".\n\n");
+//If the host with A record exists more than one time...
+if (count($hostIDs) > 1) {
+    printf("Found multiple A-Records for the Host %s\n\n", HOST);
     die("Please specify a host for which only a single A-Record exists in config.php. Exiting due to the error...\n\n");
-  }
+}
 
-  //If we couldn't determine a valid public IPv4 address
-  if (!$publicIP = getCurrentPublicIP()) {
+//If we couldn't determine a valid public IPv4 address
+if (!$publicIP = getCurrentPublicIP()) {
     die("Exiting due to fatal error...\n\n");
-  }
+}
 
-  $ipchange = False;
+$ipchange = false;
 
-  //Has the IP changed?
-  foreach ($hostIDs as $record) {
-    if ($record['destination'] != $publicIP) {
-      //Yes, it has changed.
-      $ipchange = True;
-      echo "IP has changed
-Before: " . $record['destination'] . "
-Now: " . $publicIP . "\n\n";
+//Has the IP changed?
+foreach ($hostIDs as $record) {
+    if ($record['destination'] !== $publicIP) {
+        //Yes, it has changed.
+        $ipchange = true;
+        printf("IP has changed\nBefore: %s\nNow: %s\n\n", $record['destination'], $publicIP);
+    } else {
+        //No, it hasn't changed.
+        echo "IP hasn't changed. Current IP: ".$publicIP."\n\n";
     }
-    else {
-      //No, it hasn't changed.
-      echo "IP hasn't changed. Current IP: " . $publicIP . "\n\n";
-    }
-  }
+}
 
-  //Yes, it has changed.
-  if ($ipchange == True) {
+//Yes, it has changed.
+if ($ipchange === true) {
     $hostIDs[0]['destination'] = $publicIP;
     //Update the record
     if (updateDnsRecords(DOMAIN, CUSTOMERNR, APIKEY, $apisessionid, $hostIDs)) {
-      echo "IP address updated successfully!\n\n";
+        echo "IP address updated successfully!\n\n";
+    } else {
+        die("Exiting due to fatal error...\n\n");
     }
-    else {
-      die("Exiting due to fatal error...\n\n");
-    }
-  }
+}
 
-  //Logout
-  if (logout(CUSTOMERNR, APIKEY, $apisessionid)) {
+//Logout
+if (logout(CUSTOMERNR, APIKEY, $apisessionid)) {
     echo "Logged out successfully!\n\n";
-  }
-  else {
+} else {
     die("Exiting due to fatal error...\n\n");
-  }
+}
