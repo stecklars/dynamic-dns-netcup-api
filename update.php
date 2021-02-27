79b85fe6ad31e430ab6fbb1df320e34dc97e7505
Markus Münzel <markus@muenzel.de>
2021-02-27 20:17:49 +0100

    bugfixed update logic for docker version

--- update.php
+++ update.php
@@ -15,9 +15,6 @@ $ipcache = getIPCache();
 $ipv4change = false;
 $ipv6change = false;
 
-$ipv4available = true;
-$ipv6available = true;
-
 $publicIPv4 = '127.0.0.1';
 $publicIPv6 = '::1';
 
@@ -28,17 +25,21 @@ if ($config_array['USE_IPV4'] === 'true') {
 	// get public IPv4 address
 	$publicIPv4 = $config_array['USE_FRITZBOX']  === 'true' ? getCurrentPublicIPv4FromFritzBox($config_array['FRITZBOX_IP']) : getCurrentPublicIPv4();
 
-	//If we couldn't determine a valid public IPv4 address: disable further IPv4 assessment
-	if (!$publicIPv4) {
-		$ipv4available = false;
-	} elseif ($ipcache !== false) {
-		// check whether public IPv4 has changed according to IP cache
-		if ($ipcache['ipv4'] !== $publicIPv4) {
-			$ipv4change = true;
-			outputStdout(sprintf("IPv4 address has changed according to local IP cache. Before: %s; Now: %s", $ipcache['ipv4'], $publicIPv4));
+	if ($publicIPv4) {
+		if ($ipcache !== false) {
+			// check whether public IPv4 has changed according to IP cache
+			if ($ipcache['ipv4'] !== $publicIPv4) {
+				$ipv4change = true;
+				outputStdout(sprintf("IPv4 address has changed according to local IP cache. Before: %s; Now: %s", $ipcache['ipv4'], $publicIPv4));
+			} else {
+				$ipv4change = false;
+				outputStdout("IPv4 address hasn't changed according to local IP cache. Current IPv4 address: ".$publicIPv4);
+			}
 		} else {
-			outputStdout("IPv4 address hasn't changed according to local IP cache. Current IPv4 address: ".$publicIPv4);
+			$ipv4change = true;
 		}
+	} else {
+		$ipv4change = false;
 	}
 }
 
@@ -49,22 +50,26 @@ if ($config_array['USE_IPV6'] === 'true') {
 	// get public IPv6 address
 	$publicIPv6 = getCurrentPublicIPv6($config_array['IPV6_INTERFACE'], $config_array['NO_IPV6_PRIVACY_EXTENSIONS']);
 
-	//If we couldn't determine a valid public IPv6 address: disable further IPv6 assessment
-	if (!$publicIPv6) {
-		$ipv6available = false;
-	} elseif ($ipcache !== false) {
-		// check whether public IPv6 has changed according to IP cache
-		if ($ipcache['ipv6'] !== $publicIPv6) {
-			$ipv6change = true;
-			outputStdout(sprintf("IPv6 address has changed according to local IP cache. Before: %s; Now: %s", $ipcache['ipv6'], $publicIPv6));
+	if ($publicIPv6) {
+		if ($ipcache !== false) {
+			// check whether public IPv6 has changed according to IP cache
+			if ($ipcache['ipv6'] !== $publicIPv6) {
+				$ipv6change = true;
+				outputStdout(sprintf("IPv6 address has changed according to local IP cache. Before: %s; Now: %s", $ipcache['ipv6'], $publicIPv6));
+			} else {
+				$ipv6change = false;
+				outputStdout("IPv6 address hasn't changed according to local IP cache. Current IPv6 address: ".$publicIPv6);
+			}
 		} else {
-			outputStdout("IPv6 address hasn't changed according to local IP cache. Current IPv6 address: ".$publicIPv6);
+			$ipv6change = true;
 		}
+	} else {
+		$ipv6change = false;
 	}
 }
 
-// Login to to netcup via API if public ipv4 or public ipv6 is available AND no IP cache is available or changes need to be updated
-if (($ipv6available | $ipv4available ) & ($ipcache === false | $ipv4change === true | $ipv6change === true)) {
+// Login to to netcup via API if public ipv4 or public ipv6 is available AND changes need to be updated
+if ($ipv4change === true | $ipv6change === true) {
 
 	// Login
 	if ($apisessionid = login($config_array['CUSTOMERNR'], $config_array['APIKEY'], $config_array['APIPASSWORD'], $config_array['APIURL'])) {
@@ -110,12 +115,12 @@ if (($ipv6available | $ipv4available ) & ($ipcache === false | $ipv4change === t
 	}
 
 	// update ipv4
-	if ($ipv4available) {
-		updateIP($infoDnsRecords, $publicIPv4, $apisessionid, $config_array['HOST_IPv6'], $config_array['HOST_IPv4'], $config_array['DOMAIN'], $config_array['CUSTOMERNR'], $config_array['APIKEY'], $config_array['APIURL']);
+	if ($ipv4change) {
+		updateIP($infoDnsRecords, $publicIPv4, $apisessionid, $config_array['HOST_IPv4'], $config_array['HOST_IPv4'], $config_array['DOMAIN'], $config_array['CUSTOMERNR'], $config_array['APIKEY'], $config_array['APIURL']);
 	}
 
 	// update ipv6
-	if ($ipv6available) {
+	if ($ipv6change) {
 		updateIP($infoDnsRecords, $publicIPv6, $apisessionid, $config_array['HOST_IPv6'], $config_array['HOST_IPv4'], $config_array['DOMAIN'], $config_array['CUSTOMERNR'], $config_array['APIKEY'], $config_array['APIURL']);
 	}
 
