551ebadfffad38dd3712199711f4b886dde34e45
Lars-Sören Steck <info@larssteck.de>
2022-01-19 23:44:45 +0000

    Document new changes in config

--- config.dist.php
+++ config.dist.php
@@ -1,17 +1,40 @@
 <?php
-// Enter your netcup customer number here
+
+// Enter your netcup customer number here.
 define('CUSTOMERNR', '12345');
 
-//Enter your API-Key and -Password here - you can generate them in your CCP at https://ccp.netcup.net
+
+// Enter your API-Key and -Password here - you can generate them in your CCP at https://ccp.netcup.net
 define('APIKEY', 'abcdefghijklmnopqrstuvwxyz');
 define('APIPASSWORD', 'abcdefghijklmnopqrstuvwxyz');
 
-//If set to true, the script will check for your public IPv6 address too and add it as an AAAA-Record / change an existing AAAA-Record for the host.
-//Activate this only if you have IPv6 connectivity, or you *WILL* get errors.
+
+// Define domains and subdomains which should be used for dynamic DNS in the following format:
+// domain.tld: host1, host2, host3; domain2.tld: host1, host4, *, @
+// Start with the domain (without subdomain), add ':' after the domain, then add as many subdomains as you want, seperated by ','.
+// To add another domain, finish with ';'.
+// Whitespace (spaces and newlines) are ignored. If you have a very complicated configuration, you may want to use multiple lines. Feel free to do so!
+// Subdomain configuration: Use '@' for the domain without subdomain. Use '*' for all subdomains.
+define('DOMAINLIST', myfirstdomain.com: server, dddns; myseconddomain.com: @, *, some-subdomain)
+
+
+// The old format for configuring domain + host is still supported, but deprecated. I recommend to switch to the above config,
+// as it allows you to define multiple domains + subdomains.
+
+// Enter Domain which should be used for dynamic DNS.
+// define('DOMAIN', 'mydomain.com');
+// Enter subdomain to be used for dynamic DNS, alternatively '@' for domain root or '*' for wildcard. If the record doesn't exist, the script will create it.
+// define('HOST', 'server');
+
+
+// If set to true, the script will check for your public IPv6 address too and add it as an AAAA-Record / change an existing AAAA-Record for the host.
+// Activate this only if you have IPv6 connectivity, or you *WILL* get errors.
 define('USE_IPV6', false);
 
-//If set to true, this will change TTL to 300 seconds on every run if necessary.
+
+// If set to true, this will change TTL to 300 seconds on every run if necessary.
 define('CHANGE_TTL', true);
 
-// Use netcup DNS REST-API
+
+// Use netcup DNS REST-API.
 define('APIURL', 'https://ccp.netcup.net/run/webservice/servers/endpoint.php?JSON');
